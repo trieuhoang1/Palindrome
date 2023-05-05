916ef774b81115756081a55e6566168810351d1b
Trieu Hoang <Trieu.Hoang1@alaskaair.com>
2023-05-05 13:52:22 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a word: ");
+        string input = Console.ReadLine();
+
+        if (IsPalindrome(input))
+        {
+            Console.WriteLine("The word is a palindrome.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, the word is not a palindrome :( ");
+        }
+
+        Console.ReadKey();
+    }
+
+    static bool IsPalindrome(string word)
+    {
+        int left = 0;
+        int right = word.Length - 1;
+
+        while (left < right)
+        {
+            if (word[left] != word[right])
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
